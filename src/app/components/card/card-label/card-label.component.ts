bb35045fb28693ef6243b2f4fd4fddd090ca6c24
Nickgalant <nicholasgalante@hotmail.co.uk>
2023-09-18 20:31:04 -0300

    psn store clone created

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -7,8 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardLabelComponent implements OnInit {
 
-	@Input()
-	gameLabel:string=""
+  @Input()
+  gameLabel:String=""
 
   constructor() { }
 
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,16 +6,14 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
-	@Input()
-	gameCover:string = ""
-	@Input()
-	gameLabel:string=""
-	@Input()
-	gameType:string ="XPTO | PS4"
-	@Input()
-	gamePrice:string = "R$ 399,90"
-
+  @Input()
+  gameCover:string=""
+  @Input()
+  gameLabel:String=""
+  @Input()
+  gameType:string="XPTO | PS4"
+  @Input()
+  gamePrice:string="R$ 399,90"
   constructor() { }
 
   ngOnInit(): void {
